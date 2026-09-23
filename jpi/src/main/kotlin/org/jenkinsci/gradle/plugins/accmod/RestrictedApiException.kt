6251b7d8a8a8c2c029fae18fb94031bac620b30c
Rahul Somasunderam <rahul.som@gmail.com>
2026-09-22 18:35:16 -0700

    chore: Drop jpi plugin
    We now only support jpi2. jpi stopped working with Gradle 8.13

--- jpi/src/main/kotlin/org/jenkinsci/gradle/plugins/accmod/RestrictedApiException.kt
+++
@@ -1,5 +0,0 @@
-package org.jenkinsci.gradle.plugins.accmod
-
-import java.lang.RuntimeException
-
-class RestrictedApiException : RuntimeException("Restricted APIs were detected - see https://tiny.cc/jenkins-restricted") 
